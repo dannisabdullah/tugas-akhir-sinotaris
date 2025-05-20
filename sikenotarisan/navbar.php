565e30420b622fe62c41c47eea7bc1b2ec16f5a2
dannisabdullah <dannisforstudy@gmail.com>
2025-05-20 09:28:00 +0700

    Update navbar.php

--- sikenotarisan/navbar.php
+++ sikenotarisan/navbar.php
@@ -6,7 +6,7 @@
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"/>
         </svg>
         <div class="flex-shrink-0 text-2xl font-notaris tracking-wide">
-          Notaris R. Dewi Agung
+          Notaris R. Dewi Agung SH., MKn.
         </div>
       </div>
       <div class="hidden sm:flex sm:space-x-8 relative items-center">
@@ -40,7 +40,7 @@
                 <?php endif; ?>
               </div>
               <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
-                <a href="/pakwawan/admin/index.php" class="block px-4 py-2 hover:bg-indigo-100 hover:text-indigo-700">
+                <a href="/sikenotarisan/admin/index.php" class="block px-4 py-2 hover:bg-indigo-100 hover:text-indigo-700">
                   <div class="flex items-center space-x-2">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/>
@@ -59,7 +59,7 @@
                   </div>
                 </a>
               <?php endif; ?>
-              <a href="/pakwawan/logout.php" class="block px-4 py-2 hover:bg-indigo-100 hover:text-indigo-700">Logout</a>
+              <a href="/sikenotarisan/logout.php" class="block px-4 py-2 hover:bg-indigo-100 hover:text-indigo-700">Logout</a>
             </div>
           </div>
         <?php endif; ?>
